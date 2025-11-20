d2d8193ebe0567f7ab0459b3f06be5215c984fde
notabird <notabird07@gmail.com>
2025-11-20 01:15:50 -0800

    change url to get maps so that i dont need a dumb github repo for it

--- GtagMapBrowser/MapInfo.cs
+++ GtagMapBrowser/MapInfo.cs
@@ -5,6 +5,6 @@ public class MapInfo
     public string Name { get; set; }
     public string Image { get; set; }
     public string Url { get; set; }
-    public bool Good { get; set; }
+    public bool Verified { get; set; }
     public bool HasLua { get; set; }
 }
